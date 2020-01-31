61e95912fd720e0577f131ee82f863e13d69ed6c
Acharya Sharad <sharad.acharya@lexisnexis.com>
2020-01-31 15:20:16 -0500

    genesis of the file

---
+++ mle2e/data_understanding.py
@@ -0,0 +1,15 @@
+"""The file is contains utility classes and methods to read data and run some basic data checks
+
+"""
+
+#This file has dependency to following python libraries:
+#  Pandas, Numpy, sklearn,
+
+import numpy as np
+
+
+class DataUnderstanding:
+
+    def __init__(self):
+        pass
+
