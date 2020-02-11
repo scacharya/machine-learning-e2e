7ab7f280177daf90dfa8e4f96f94783411d17033
Acharya Sharad <sharad.acharya@lexisnexis.com>
2020-02-11 10:43:50 -0500

    refactor

--- mle2e/data_source.py
+++ mle2e/data_source.py
@@ -1,46 +1,98 @@
-import pandas as pd
 import datatable as dtbl
-import urllib.error as ue
-
+import os as os
+from pathlib import Path
+import zipfile, urllib.request, shutil, tarfile
 
 """
-The class file to encapsulate **ALL** type of data sources
+The file handles **ALL** type of data sources (wish list)
 The datatable import $ pip install datatable. If this command fails for newer version of python then
- Run following directly from git repo $ pip install git+https://github.com/h2oai/datatable
+ un following directly from git repo $ pip install git+https://github.com/h2oai/datatable
 """
 
-class MLE2EDS:
 
-    def __init__(self, source):
-        self.data_source = source
-        self.data_frame = None
-        self.data_table = None
-
-
-    """
-    Assuming the source string as https://some-site.com/data/file-name.ext
-    1) check if the soruce string ends with .csv, then read it as is(remote or local) 
-    2) for other file types, tgz, zip or .gzip, then a)download b) extract and c) read to dataframe 
-    """
-
-    def read_df_csv(self):
-        if self.data_source.upper().endswith(".CSV"):
-            try:
-                self.data_frame = pd.read_csv(self.data_source)
-                return self.data_frame
-            except ue.URLError as urlerr:
-                print("URLError occurred trying to connect", urlerr)
-        else:
-            print("only .csv files are supported, use datatable instead ")
+"""
+Utility to get data based on various operation 
+This function is only needed for the calling functions and pass the *known* data source
+DATA_URL = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.tgz"
+"""
+def get_data(file_name):
+    check_data_dir()
+    return check_data_file(file_name)
 
 
-    """
-    datatable reads much faster, file with multiple delimeter and different format
-    """
-    def read_datatable(self):
-        self.data_table=dtbl.fread(self.data_source)
-        return self.data_table
+"""
+check if data directory exists under the workspace, create one if it does not
+"""
+def check_data_dir():
+    thispath = os.getcwd()+"/data"
+    if not os.path.exists(path=thispath):
+        os.makedirs(thispath, 0o777, False )
 
 
-    def show_summary(self):
+"""
+check if the data file already exists locally, if not download
+if the downloaded data is compressed, decompress it
+"""
+def check_data_file(url):
+    if url.upper().endswith(".ZIP") or url.upper().endswith(".TGZ"):
+        return check_and_download_acrhive_file(url)
+    elif url.upper().endswith(".CSV"):
+        return check_and_download_csv_file(url)
+    else:
         pass
+
+"""
+check for remote zip files, download and extract if does not exist locally
+"""
+def check_and_download_acrhive_file(someurl):
+    url_split = someurl.split("/")
+    last_elem = len(url_split)-1
+    file_to_download = url_split[last_elem]
+    datadir = os.getcwd() + "/data/"
+    file_path = Path(datadir+file_to_download)
+    if file_path.is_file():
+        print("File exists in data directory, not downloading")
+    else:
+        print("File does not exist in data directory, downloading and extracting...")
+        if someurl.upper().endswith(".ZIP"):
+            with urllib.request.urlopen(someurl) as response, open(file_path, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+                with zipfile.ZipFile(file_path) as zf:
+                    os.chdir(datadir)
+                    zf.extractall()
+        elif someurl.upper().endswith(".TGZ"):
+            with urllib.request.urlopen(someurl) as response, open(file_path, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+                tar = tarfile.open(datadir+file_to_download, 'r')
+                os.chdir(datadir)
+                for item in tar:
+                    tar.extract(item)
+        else:
+            pass
+    return file_path
+
+"""
+Check and download csv 
+"""
+def check_and_download_csv_file(url):
+    url_split = url.split("/")
+    last_elem = len(url_split) - 1
+    file_to_download = url_split[last_elem]
+    datadir = os.getcwd() + "/data/"
+    file_path = Path(datadir + file_to_download)
+    response = urllib.request.urlopen(url)
+    with open(os.path.join(datadir, file_to_download), 'w+b') as f:
+        f.write(response.read())
+    return file_path
+
+"""
+Read the file using datatable 
+"""
+def load_data(filename):
+    ##try opening .csv file instad
+    try:
+        filetoopen = str(filename).replace(".tgz", ".csv",1)
+        data_table = dtbl.fread(filetoopen, fill=True)
+        return data_table
+    except RuntimeError as re:
+        print("Unable to read the file into datatable, trying to downlaod", re)
--- mle2e/data_understanding.py
+++ mle2e/data_understanding.py
@@ -1,10 +1,13 @@
 """
 Utility to consolidate the data into a single class for step #1) Data Understanding in  CRIPS-DM methodology
 """
-
 from mle2e import data_source as ds
 
 DATA_URL = "https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
-data_source = ds.MLE2EDS(DATA_URL)
+DATA_URL = "https://ndownloader.figshare.com/files/5976036"
+DATA_URL = "http://lib.stat.cmu.edu/datasets/houses.zip"
+DATA_URL = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.tgz"
 
-data_source.show_summary()
+local_file = ds.get_data(DATA_URL)
+summary_dtbl = ds.load_data(local_file)
+print(summary_dtbl.to_pandas().head(50))
